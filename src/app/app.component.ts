c4bd58430a617916a2c6085ab79681c0cd2eeae4
Caroline Kershaw <75424622+CaroKershaw@users.noreply.github.com>
2022-04-11 11:38:38 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { NgModule, Component, enableProdMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { DxDataGridModule, DxButtonModule } from 'devextreme-angular';
+import { Service, Employee, State } from './app.service';
+
+
+@Component({
+  selector: 'demo-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [Service],
+})
+export class AppComponent {
+  dataSource: Employee[];
+
+  states: State[];
+
+  events: Array<string> = [];
+
+  constructor(service: Service) {
+    this.dataSource = service.getEmployees();
+    this.states = service.getStates();
+  }
+
+  logEvent(eventName) {
+    this.events.unshift(eventName);
+  }
+
+  clearEvents() {
+    this.events = [];
+  }
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    DxDataGridModule,
+    DxButtonModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
